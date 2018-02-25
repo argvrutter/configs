1523c207cda8b6e19ee6399916a4d280dd6377d9
Aiden <solsane@gmail.com>
2018-02-24 22:38:11 -0500

    changes are a comin, it's about time a sherriff enforces universal dev environments around here

---
+++ fresh_init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+if[$1 == "su"]; 
+    mkdir $HOME/install
+    cd $HOME/install
+
+    #add PPA's
+    sudo add-apt-repository ppa:inkscape.dev/stable ppa:webupd8team/atom
+
+    #update
+    apt update
+    sudo apt full-upgrade
+    #install apt packages
+    apt install screen tmux vim htop chromium-browser zsh g++ lcov git libmagickcore-dev libusb-1.0.0-dev libnss3-dev libglib2.0-dev libtool fprintd automake docker.io python python3 python-pip python-pip3 npm doxygen inkscape sl cmatrix term2
+    #update
+    apt update
+    #non apt packages
+    wget https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh
+    bash Anaconda-latest-Linux-x86_64.sh
+    dpkg -i keybase_amd64.deb
+    apt-get install -f
+    wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+    dpkg -i gitkraken-amd64.deb
+    pip install setuptools
+    
+    wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+    dpkg -i /path/to/discord.deb
+fi
+#install apm packages
+apm install atom-clock file-icons linter git-plus docblok atom-gdb-debugger language-cmake minimap teletype
+#desktop environment
+chsh -s $(which zsh)
+
+#initialize dotfiles
+cd $HOME
+git clone https://github.com/solsane/configs && cd dotfiles && ./install

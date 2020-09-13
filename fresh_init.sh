f3d1cc072eb419513871a6ae074816961ef537c4
solsane <solsane@pop-os.localdomain>
2020-09-13 15:11:18 -0400

    troubleshooting changes; install p10k

--- fresh_init.sh
+++ fresh_init.sh
@@ -12,21 +12,13 @@
     sudo apt full-upgrade
     #install apt packages
     apt install screen fonts-powerline tmux vim htop chromium-browser zsh g++ lcov git libmagickcore-dev libusb-1.0.0-dev libnss3-dev libglib2.0-dev libtool fprintd automake python python3 python-pip npm doxygen inkscape sl cmatrix
-    #update
-    apt update
-    #non apt packages
-    wget https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh
-    bash Anaconda-latest-Linux-x86_64.sh
-    dpkg -i keybase_amd64.deb
-    apt-get install -f
-    wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-    dpkg -i gitkraken-amd64.deb
-    pip install setuptools
+    #updategit@github.com:solsane/configs.git
 
     wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
     dpkg -i /path/to/discord.deb
 
 #install apm packages
+# install vscode packages
 apm install atom-clock file-icons linter git-plus docblok atom-gdb-debugger language-cmake minimap teletype
 #desktop environment
 chsh -s $(which zsh)
